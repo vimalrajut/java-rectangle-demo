177a306676e4f24c7cde6ad7bf971ef6bcb18273
root <root@ip-172-31-18-127.ap-south-1.compute.internal>
2018-05-28 18:01:39 +0000

    changes in src folder

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,9 +2,9 @@ public class Rectangle {
   public int length;
   public int width;
 
-  public Rectangle(int length, int width) {
-    this.length = length;
-    this.width = width;
+  public Rectangle(int length1, int width1) {
+    this.length = length1;
+    this.width = width1;
   }
 
   public int getArea() {
